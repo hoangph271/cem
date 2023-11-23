1cbbf99a89770e39c4d8b825b35471ca203ff039
Snêu <hoangph271@gmail.com>
2023-11-24 00:29:05 +0700

    .

--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -8,12 +8,84 @@ class SampleItemDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Item Details'),
-      ),
-      body: const Center(
-        child: Text('More Information Here'),
+    return const Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('Item Details'),
+      // ),
+      body: ViewerGestureDetector(),
+    );
+  }
+}
+
+class ViewerGestureDetector extends StatelessWidget {
+  const ViewerGestureDetector({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+            context: context,
+            builder: (builder) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(8, 16, 8, 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton.icon(
+                      icon: const Icon(
+                        Icons.lock,
+                        size: 32,
+                      ),
+                      label: const Text('Lock'),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    ElevatedButton.icon(
+                      icon: const Icon(
+                        Icons.pause,
+                        size: 32,
+                      ),
+                      label: const Text('Lock'),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    ElevatedButton.icon(
+                      icon: const Icon(
+                        Icons.shuffle,
+                        size: 32,
+                      ),
+                      label: const Text('Suffle'),
+                      onPressed: () => Navigator.pop(context),
+                    )
+                  ],
+                ),
+              );
+            });
+      },
+      child: const Gallery(),
+    );
+  }
+}
+
+class Gallery extends StatelessWidget {
+  const Gallery({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.black),
+      child: SafeArea(
+        child: Center(
+          child: FittedBox(
+            fit: BoxFit.fill,
+            child: Image.network(
+              'https://www.runicgames.com/images/torchlight2/tl2-marquee-hero@2x.png',
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'settings_controller.dart';
 
@@ -17,33 +18,45 @@ class SettingsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Settings'),
+        title: Text(
+          'Settings',
+          style: GoogleFonts.ubuntuMono(),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        // Glue the SettingsController to the theme selection DropdownButton.
-        //
-        // When a user selects a theme from the dropdown list, the
-        // SettingsController is updated, which rebuilds the MaterialApp.
-        child: DropdownButton<ThemeMode>(
-          // Read the selected themeMode from the controller
-          value: controller.themeMode,
-          // Call the updateThemeMode method any time the user selects a theme.
-          onChanged: controller.updateThemeMode,
-          items: const [
-            DropdownMenuItem(
-              value: ThemeMode.system,
-              child: Text('System Theme'),
-            ),
-            DropdownMenuItem(
-              value: ThemeMode.light,
-              child: Text('Light Theme'),
-            ),
-            DropdownMenuItem(
-              value: ThemeMode.dark,
-              child: Text('Dark Theme'),
-            )
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          // Glue the SettingsController to the theme selection DropdownButton.
+          //
+          // When a user selects a theme from the dropdown list, the
+          // SettingsController is updated, which rebuilds the MaterialApp.
+          child: Column(
+            children: [
+              const Image(
+                  image: NetworkImage(
+                      'https://www.runicgames.com/images/hob-marquee-tower.jpg')),
+              DropdownButton<ThemeMode>(
+                // Read the selected themeMode from the controller
+                value: controller.themeMode,
+                // Call the updateThemeMode method any time the user selects a theme.
+                onChanged: controller.updateThemeMode,
+                items: const [
+                  DropdownMenuItem(
+                    value: ThemeMode.system,
+                    child: Text('System Theme'),
+                  ),
+                  DropdownMenuItem(
+                    value: ThemeMode.light,
+                    child: Text('Light Theme'),
+                  ),
+                  DropdownMenuItem(
+                    value: ThemeMode.dark,
+                    child: Text('Dark Theme'),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
