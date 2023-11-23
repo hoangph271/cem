c35827c54322a256a47231f6c794e2cd3c9534df
Snêu <hoangph271@gmail.com>
2023-11-24 01:56:09 +0700

    ModalBottomSheet works (kinda)

--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-/// Displays detailed information about a SampleItem.
 class SampleItemDetailsView extends StatelessWidget {
   const SampleItemDetailsView({super.key});
 
@@ -9,58 +8,118 @@ class SampleItemDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Item Details'),
-      // ),
       body: ViewerGestureDetector(),
     );
   }
 }
 
-class ViewerGestureDetector extends StatelessWidget {
+class ViewerGestureDetector extends StatefulWidget {
   const ViewerGestureDetector({
     super.key,
   });
 
   @override
+  State<ViewerGestureDetector> createState() => _ViewerGestureDetectorState();
+}
+
+class _ViewerGestureDetectorState extends State<ViewerGestureDetector> {
+  bool _isLocked = false;
+  bool _isPaused = false;
+  bool _isShuffle = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
+        final disabledColor = Theme.of(context).disabledColor;
+
         showModalBottomSheet(
             context: context,
             builder: (builder) {
-              return Padding(
-                padding: const EdgeInsets.fromLTRB(8, 16, 8, 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton.icon(
-                      icon: const Icon(
-                        Icons.lock,
-                        size: 32,
-                      ),
-                      label: const Text('Lock'),
-                      onPressed: () => Navigator.pop(context),
+              return StatefulBuilder(builder: (context, setState) {
+                void handleLockPressed() {
+                  setState(() {
+                    _isLocked = !_isLocked;
+                  });
+
+                  if (_isLocked) {
+                    Navigator.pop(context);
+                  }
+                }
+
+                void handlePlayPausePressed() {
+                  setState(() {
+                    _isPaused = !_isPaused;
+                  });
+                }
+
+                void handleShufflePressed() {
+                  setState(() {
+                    _isShuffle = !_isShuffle;
+                  });
+                }
+
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 16, 8, 16),
+                  child: IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            IconButton(
+                                onPressed: handleLockPressed,
+                                color: _isLocked
+                                    ? Theme.of(context).colorScheme.tertiary
+                                    : null,
+                                icon: const Icon(
+                                  Icons.lock,
+                                  size: 32,
+                                )),
+                            if (_isLocked)
+                              const Text('Unlock')
+                            else
+                              const Text('Lock'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            IconButton(
+                                onPressed:
+                                    _isLocked ? null : handlePlayPausePressed,
+                                icon: Icon(
+                                    _isPaused ? Icons.play_arrow : Icons.pause,
+                                    size: 32)),
+                            Text(_isPaused ? 'Play' : 'Pause',
+                                style: _isLocked
+                                    ? TextStyle(color: disabledColor)
+                                    : null)
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            IconButton(
+                              onPressed:
+                                  _isLocked ? null : handleShufflePressed,
+                              icon: const Icon(
+                                Icons.shuffle,
+                                size: 32,
+                              ),
+                              color: _isShuffle
+                                  ? Theme.of(context).colorScheme.tertiary
+                                  : null,
+                            ),
+                            Text('Shuffle',
+                                style: _isLocked
+                                    ? TextStyle(color: disabledColor)
+                                    : null)
+                          ],
+                        ),
+                      ],
                     ),
-                    ElevatedButton.icon(
-                      icon: const Icon(
-                        Icons.pause,
-                        size: 32,
-                      ),
-                      label: const Text('Lock'),
-                      onPressed: () => Navigator.pop(context),
-                    ),
-                    ElevatedButton.icon(
-                      icon: const Icon(
-                        Icons.shuffle,
-                        size: 32,
-                      ),
-                      label: const Text('Suffle'),
-                      onPressed: () => Navigator.pop(context),
-                    )
-                  ],
-                ),
-              );
+                  ),
+                );
+              });
             });
       },
       child: const Gallery(),
